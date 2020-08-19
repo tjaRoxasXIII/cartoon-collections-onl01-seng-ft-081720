f8123ab801a55e8b675757f5f0ddfd3bfe27cf2c
Taylor Allen <tjaroguetech@gmail.com>
2020-08-19 19:43:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,6 +21,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |cheese|
-    array.include?(cheese)
+    cheese_types.include?(cheese)
   end
 end
